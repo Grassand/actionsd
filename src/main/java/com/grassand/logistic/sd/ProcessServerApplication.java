0fe87d3111af6247d74c493e621726f85f11a7ae
grassand <grassandtech@gmail.com>
2019-07-22 22:42:35 +0530

    inittal commit

---
+++ src/main/java/com/grassand/logistic/sd/ProcessServerApplication.java
@@ -0,0 +1,15 @@
+package com.grassand.logistic.sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ProcessServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessServerApplication.class, args);
+	}
+
+}
