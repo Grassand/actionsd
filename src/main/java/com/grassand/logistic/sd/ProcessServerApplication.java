8c828b063411dcff58ded18dd51a474a44a3270f
grassand <grassandtech@gmail.com>
2019-08-04 10:44:35 +0530

    main and gradle

--- src/main/java/com/grassand/logistic/sd/ProcessServerApplication.java
+++ src/main/java/com/grassand/logistic/sd/ProcessServerApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+//@EnableEurekaServer
 @SpringBootApplication
 public class ProcessServerApplication {
 
 	/**
-	 * Main Method
+	 * Main Method we shall see
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessServerApplication.class, args);
